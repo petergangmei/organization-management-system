a0abff78802e808bde5c4d8d42af0c48475190e9
Peter Gangmei <topeter44msg@gmail.com>
2024-07-10 12:08:28 +0530

    New Update

--- core/settings/common.py
+++ core/settings/common.py
@@ -15,7 +15,7 @@ SECRET_KEY = 'django-insecure-1v7i3x%0fw)&qdo2&s9pcfc-+$op4njzuu)mg^#ta9l*qv2&a+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 
-DEBUG=True
+DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
